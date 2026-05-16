f97573708b82fb6b1b51e8d0d04f3ec087a1584c
Guilherme Camargo <guilherme.paula@sptech.school>
2026-05-16 19:54:20 -0300

    Criação da tabela desempenho e inserts

--- src/database/script-bd.sql
+++ src/database/script-bd.sql
@@ -2,39 +2,27 @@ create database projetoindividual;
 use projetoindividual;
 
 create table usuario (
-idUsuario int primary key auto_increment,
-nome varchar (30),
-email varchar (30),
-genero varchar (30),
-senha varchar (30)
+    idUsuario int primary key auto_increment,
+    nome varchar(100) not null,
+    email varchar(100) not null unique,
+    genero varchar(30) not null,
+    senha varchar(100) not null
 );
 
 create table clube (
-idTime int primary key auto_increment,
-nome varchar (30)
+    idTime int primary key auto_increment,
+    nome varchar(50) not null
 );
 
 create table desempenho (
-idPartidas int primary key auto_increment,
-partidas int,
-gols int,
-assistencias int,
-fk_time int,
-constraint fk_time
-foreign key (fk_time) references clube (idTime)
+    idPartidas int primary key auto_increment,
+    partidas int,
+    gols int,
+    assistencias int,
+    fk_time int,
+    constraint fk_time
+        foreign key (fk_time) references clube(idTime)
 );
-
-create table conquistas (
-idConquista int primary key auto_increment,
-premio varchar (20),
-ano year,
-fk_clube int,
-constraint fk_clube
-foreign key (fk_clube) references clube (idTime)
-);
-
-insert into usuario values 
-(default, "Gui", "gui@gmail.com", "Masculino", "1234Gui");
 
 insert into clube values 
 (default, "Real Madrid");
@@ -42,9 +30,4 @@ insert into clube values
 insert into desempenho values
 (default, 438, 450, 131, 1);
 
-insert into conquistas values 
-(default, "Bola de ouro", 2014, 1);
-
-select * from conquistas;
-
-
+select * from usuario;
