5f2bdfef5612f3417b7052cea5c2c13ef0c0a1d7
Guilherme Camargo <guilherme.paula@sptech.school>
2026-05-16 19:57:45 -0300

    Configuração do desempenho

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
+var desempenhoRouter = require("./src/routes/desempenhoRouter");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,6 +26,7 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
+app.use("/", desempenhoRouter);
 
 app.listen(PORTA_APP, function () {
 
