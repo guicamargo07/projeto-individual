89eea7e65a3c94f3ac775d65eb7c0e9b28cb42b5
Guilherme Camargo <guilherme.paula@sptech.school>
2026-05-26 22:12:04 -0300

    atualização do cadastro

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -6,24 +6,18 @@ function autenticar(req, res) {
     var senha = req.body.senhaServer;
 
     if (email == undefined) {
-
         res.status(400).send("Seu email está undefined!");
-
     } else if (senha == undefined) {
-
         res.status(400).send("Sua senha está indefinida!");
-
     } else {
 
         usuarioModel.autenticar(email, senha)
-
             .then(function (resultadoAutenticar) {
 
                 console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
                 console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
 
                 if (resultadoAutenticar.length == 1) {
-
                     res.json({
                         id: resultadoAutenticar[0].id,
                         nome: resultadoAutenticar[0].nome,
@@ -31,30 +25,17 @@ function autenticar(req, res) {
                         genero: resultadoAutenticar[0].genero,
                         senha: resultadoAutenticar[0].senha
                     });
-
                 } else if (resultadoAutenticar.length == 0) {
-
                     res.status(403).send("Email e/ou senha inválido(s)");
-
                 } else {
-
                     res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-
                 }
 
             })
-
             .catch(function (erro) {
-
                 console.log(erro);
-
-                console.log(
-                    "\nHouve um erro ao realizar o login! Erro: ",
-                    erro.sqlMessage
-                );
-
+                console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
                 res.status(500).json(erro.sqlMessage);
-
             });
     }
 }
@@ -64,48 +45,67 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var genero = req.body.generoServer;
+    var jogador = req.body.jogadorServer;
+    var posicao = req.body.posicaoServer;
+    var clube = req.body.clubeServer;
     var senha = req.body.senhaServer;
 
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
-
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
-
     } else if (genero == undefined) {
         res.status(400).send("Seu gênero está undefined!");
-
+    } else if (jogador == undefined) {
+        res.status(400).send("Jogador está undefined!");
+    } else if (posicao == undefined) {
+        res.status(400).send("Posição está undefined!");
+    } else if (clube == undefined) {
+        res.status(400).send("Clube está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-
     } else {
 
-        usuarioModel.cadastrar(nome, email, genero, senha)
-
+        usuarioModel.cadastrar(nome, email, genero, jogador, posicao, clube, senha)
             .then(function (resultado) {
-
                 res.json(resultado);
-
             }).catch(function (erro) {
-
                 console.log(erro);
-
-                console.log(
-                    "\nHouve um erro ao realizar o cadastro! Erro: ",
-                    erro.sqlMessage
-                );
-
+                console.log("\nHouve um erro ao realizar o cadastro! Erro: ", erro.sqlMessage);
                 if (erro.code === 'ER_DUP_ENTRY') {
                     res.status(409).send("Email já cadastrado!");
                 } else {
                     res.status(500).json(erro.sqlMessage);
                 }
+            });
+    }
+}
 
+function perfil(req, res) {
+
+    var email = req.query.email;
+
+    if (email == undefined) {
+        res.status(400).send("Email está undefined!");
+    } else {
+
+        usuarioModel.buscarPorEmail(email)
+            .then(function (resultado) {
+                if (resultado.length == 1) {
+                    res.json(resultado[0]);
+                } else {
+                    res.status(404).send("Usuário não encontrado!");
+                }
+            })
+            .catch(function (erro) {
+                console.log(erro);
+                res.status(500).json(erro.sqlMessage);
             });
     }
 }
 
 module.exports = {
     autenticar,
-    cadastrar
+    cadastrar,
+    perfil
 }
