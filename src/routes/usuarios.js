053a8697e12944630e1ca17a4f96948ed51c5101
Guilherme Camargo <guilherme.paula@sptech.school>
2026-05-26 22:13:57 -0300

    atualização no perfil

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -5,10 +5,14 @@ var usuarioController = require("../controllers/usuarioController");
 
 router.post("/cadastrar", function (req, res) {
     usuarioController.cadastrar(req, res);
-})
+});
 
 router.post("/autenticar", function (req, res) {
     usuarioController.autenticar(req, res);
+});
+
+router.get("/perfil", function (req, res) {
+    usuarioController.perfil(req, res);
 });
 
 module.exports = router;
